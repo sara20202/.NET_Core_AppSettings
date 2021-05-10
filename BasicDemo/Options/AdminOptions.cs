ecf7b80e78e0a16b0008e532b9cbb19080b73f1f
sara20202 <julian.sara.2020@gmail.com>
2021-05-10 10:49:28 +0200

    some init msg

---
+++ BasicDemo/Options/AdminOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicDemo.Options
+{
+    public class AdminOptions
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ BasicDemo/Options/EmailSettingsOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicDemo.Options
+{
+    public class EmailSettingsOptions
+    {
+        public bool EnableEmailSystem { get; set; }
+       
+        public int EmailTimeoutInSeconds { get; set; }
+        public List<string> EmailServices { get; set; } = new List<string>();
+        public AdminOptions EmailAdmin { get; set; } = new AdminOptions();
+    }
+}
